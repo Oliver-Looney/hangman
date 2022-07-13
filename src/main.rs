29b9d6827dcddf20c03cc4b3312b26d32a798349
Oliver-Looney <88001922+Oliver-Looney@users.noreply.github.com>
2022-07-13 15:37:39 +0100

    added basic menu

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,34 @@ mod utils;
 use utils::pre_game_set_up;
 
 fn main() {
+    let mut user_input = 'y';
+    println!("Welcome to Hangman\ngood luck!");
+    while user_input != 'q'{
+        println!("\nPlay a game?\nEnter any character to play or q to quit:");
+        user_input = utils::read_user_input_character();
+        if user_input == 'q'{
+            break;
+        }
+        game_loop();
+    }
+}
+
+fn game_loop() {
     let hangman_pics = pre_game_set_up::get_drawings();
     let mut lives = 0;
     let mut answer = pre_game_set_up::get_game_answer();
 
-    println!("This is hangman game!");
+    println!("New hangman game!");
     let mut flag = false;
-    while !flag & (lives<hangman_pics.len()-1){
+    while !flag & (lives < hangman_pics.len() - 1) {
         // print start & blanks
         println!("{}", hangman_pics[lives]);
         println!("{}", utils::get_user_answer(&answer));
         let guess = utils::read_user_input_character();
         flag = true;
         let mut change = false;
-        for i in 0..answer.len(){
-            if answer[i].letter == guess{
+        for i in 0..answer.len() {
+            if answer[i].letter == guess {
                 answer[i].revealed = true;
                 change = true;
             }
